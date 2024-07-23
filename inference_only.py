0177309913be7e9df721748d79423adaf2ae01a7
Anvar <81307284+anvarcool@users.noreply.github.com>
2024-07-23 11:20:40 +0500

    Update inference_only.py

--- inference_only.py
+++ inference_only.py
@@ -17,7 +17,7 @@ filenames_test = np.array(annotations_test['attachment_id'])
 file_labels_test = np.array([labels_map[key] for key in annotations_test['text']])
 model = LSTMClassification(128, models_path)
 test = MyDataset_no_augment(filenames_test, file_labels_test, False, data_path)
-testloader = torch.utils.data.DataLoader(test)
+testloader = torch.utils.data.DataLoader(test, batch_size = 40)
 model.load_state_dict(torch.load(f'{models_path}/final_model_weights.pth'))
 evaluate_RNN(model, testloader)
 
