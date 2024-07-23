836d45d6adbe41e57ef639ae3aca309af56fcd5e
Anvar <81307284+anvarcool@users.noreply.github.com>
2024-07-23 11:05:45 +0500

    Update inference_only.py

--- inference_only.py
+++ inference_only.py
@@ -4,8 +4,8 @@ from create_datasets import MyDataset_no_augment
 import numpy as np
 import pandas as pd
 import torch
+models_path = input('Print path to data:')
 data_path = input('Print path to data:')
-pretrained_weights_path = input('Print path to pretrained_on_ASL_model:')
 annotations = pd.read_csv(f'{data_path}/annotations.csv', sep = '\t')
 annotations_train = annotations.query('train & (text != "no_event")')
 annotations_test = annotations.query('~train & (text != "no_event")')
@@ -15,9 +15,10 @@ filenames_train = np.array(annotations_train['attachment_id'])
 file_labels_train = np.array([labels_map[key] for key in annotations_train['text']])
 filenames_test = np.array(annotations_test['attachment_id'])
 file_labels_test = np.array([labels_map[key] for key in annotations_test['text']])
-model = LSTMClassification(128, pretrained_weights_path)
+model = LSTMClassification(128, models_path)
 test = MyDataset_no_augment(filenames_test, file_labels_test, False, data_path)
 testloader = torch.utils.data.DataLoader(test)
-model.load_state_dict(torch.load(f'{pretrained_weights_path}/final_model_weights.pth'))
+model.load_state_dict(torch.load(f'{models_path}/final_model_weights.pth'))
 evaluate_RNN(model, testloader)
+
 predicts = model.inference(testloader)
